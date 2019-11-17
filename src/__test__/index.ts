d9d4c05d4ba63101b437e12ddaae5aad4781a7f5
pengxiang <923164250@qq.com>
2019-11-18 00:20:09 +0800

    feat: unit test

--- src/__test__/index.ts
+++ src/__test__/index.ts
@@ -11,5 +11,5 @@ dslToJoi(controllerDsls, 'src/__tmp__');
 //   const val = Math.pow(1 + x, 1) + Math.pow(1 + x, 2) + Math.pow(1 + x, 3) + Math.pow(1 + x, 4) + Math.pow(1 + x, 5);
 //   console.log(x.toFixed(2), val.toFixed(4));
 // }
-const joi = require('../../../../src/__tmp__/joi');
-console.log(joi);
+// const joi = require('../../../../src/__tmp__/joi');
+// console.log(joi);
